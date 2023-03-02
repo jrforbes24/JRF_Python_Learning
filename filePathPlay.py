148025ecfbeca478438ce1dbc87c1b72b7c0ab23
John R. Forbes <jrforbes24@gmail.com>
2023-03-02 06:28:56 -0700

    learning about using os.path

--- filePathPlay.py
+++ filePathPlay.py
@@ -1,3 +1,23 @@
 import os
-theFilePathResults = os.path.join('C:','Users','johnr','Cindy')
-print(theFilePathResults)
+import getpass
+theFilePathResults = os.listdir('C:\\Users\\johnr\\Documents\\GitHub\\JRF_Python_Learning')
+totalFileSize = 0
+for i in theFilePathResults:
+    if os.path.isfile(i):
+        totalFileSize += os.path.getsize(i)
+        print(i)   
+
+
+print(totalFileSize)
+
+totalFileSize = 0
+for filename in os.listdir('C:\\Users\\johnr\\Documents\\GitHub\\JRF_Python_Learning'):
+    if not os.path.isfile(os.path.join('C:\\Users\\johnr\\Documents\\GitHub\\JRF_Python_Learning', filename )):
+        continue
+    totalFileSize += os.path.getsize(os.path.join('C:\\Users\\johnr\\Documents\\GitHub\\JRF_Python_Learning', filename ))
+    print(filename)
+
+print(totalFileSize)
+
+user = getpass.getuser()
+print(user)
