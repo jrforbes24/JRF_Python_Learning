773c937ea720ad33646a7288e3375bd2ecdfc6d3
John R. Forbes <jrforbes24@gmail.com>
2023-03-02 20:11:59 -0700

    doing some work with open and shelve

--- files.py
+++ files.py
@@ -1,4 +1,32 @@
-myTrans = open('C:\\Users\\johnr\\Documents\\GitHub\\JRF_Python_Learning\\TestArtifacts\\transactions.txt')
-theContent = myTrans.readlines()
-print(theContent)
+import shelve
+
+myTrans = open('C:\\Users\\johnr\\Documents\\GitHub\\JRF_Python_Learning\\TestArtifacts\\newTestFile.txt', 'w')
+myTrans.write('hello world, this is john, again. the first time..\n')
+myTrans.write('hello world, this is john, again the second time...\n')
+""" for line in theContent:
+    theList = line.split(',')
+    print(theList[3], '\n') """
 myTrans.close()
+
+baconFile = open('C:\\Users\\johnr\\Documents\\GitHub\\JRF_Python_Learning\\TestArtifacts\\bacon.txt', 'w')
+baconFile.write('Bacon is not a vegetable.')
+baconFile.close()
+
+baconFile = open('C:\\Users\\johnr\\Documents\\GitHub\\JRF_Python_Learning\\TestArtifacts\\bacon.txt', 'a')
+baconFile.write('\n\n Bacon is deliciosa.')
+baconFile.close()
+
+shelfFile = shelve.open('mydata')
+shelfFile['cats'] = ['Garfield','Simon','Dawg','Hairless']
+shelfFile.close()
+
+shelfFile = shelve.open('mydata')
+print(shelfFile['cats'])
+shelfFile.close()
+
+shelfFile = shelve.open('mydata')
+print(list(shelfFile.keys()))
+print(list(shelfFile.values()))
+shelfFile.close()
+
+
