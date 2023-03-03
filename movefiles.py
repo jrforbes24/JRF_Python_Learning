3c8f6dbf32772c447b314648d5546266607a1aa5
John R. Forbes <jrforbes24@gmail.com>
2023-03-03 08:15:06 -0700

    moving files to test shutil

---
+++ movefiles.py
@@ -0,0 +1,10 @@
+import shutil
+
+# shutil.copy('C:\\Users\\johnr\\Documents\\GitHub\\JRF_Python_Learning\\spam.txt','TestArtifacts\\newSpam.txt')
+
+# shutil.copytree('TestArtifacts','C:\\Users\\johnr\\TestArtifacts')
+# shutil.move('TestArtifacts\\bacon.txt','baconRules.txt')
+
+""" shutil.move('baconRules.txt', 'TestArtifacts\\bacon.txt')
+shutil.move('eggsAndBacon.txt', 'TestArtifacts\\eggs.txt')
+shutil.move('spam.txt', 'TestArtifacts\\spam.txt') """
