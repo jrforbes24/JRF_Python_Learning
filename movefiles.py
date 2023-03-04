f8ddea38e5a6df023a2b2a115f460677389a502b
John R. Forbes <jrforbes24@gmail.com>
2023-03-03 17:51:30 -0700

    playing with removing and moving files.

--- movefiles.py
+++ movefiles.py
@@ -1,4 +1,6 @@
 import shutil
+import os
+import send2trash
 
 # shutil.copy('C:\\Users\\johnr\\Documents\\GitHub\\JRF_Python_Learning\\spam.txt','TestArtifacts\\newSpam.txt')
 
@@ -8,3 +10,24 @@ import shutil
 """ shutil.move('baconRules.txt', 'TestArtifacts\\bacon.txt')
 shutil.move('eggsAndBacon.txt', 'TestArtifacts\\eggs.txt')
 shutil.move('spam.txt', 'TestArtifacts\\spam.txt') """
+
+# os.unlink('TestArtifacts\\eggs.txt')
+""" try:
+    os.rmdir('TestArtifacts\\SubFolder')
+except:
+    print('Folder had content. Using shutil.rmtree.')
+    shutil.rmtree('TestArtifacts\\SubFolder') """
+ 
+""" os.chdir('TestArtifacts\\SubFolder')
+for filename in os.listdir():
+    if filename.endswith('.txt'):
+        # os.unlink(filename)
+        print(filename) """
+
+# send2trash.send2trash('TestArtifacts\\SubFolder')
+
+
+
+
+        
+        
