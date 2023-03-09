aa2d6c2dd7373902b9d30272a784d9dfa74f41c3
John R. Forbes <jrforbes24@gmail.com>
2023-03-08 20:04:22 -0700

    working on try,except and assertion

--- debugging.py
+++ debugging.py
@@ -1,3 +1,6 @@
+import traceback
+import os
+
 """
 Make a box with the given character, height and width. 
 
@@ -11,12 +14,20 @@ Make a box with the given character, height and width.
 """
 
 def makeBox(character, height, width):
-    if len(character) != 1:
-        raise Exception("'character' needs to be a string of length 1")
-    print(character * width)
-    for i in range(height - 2):
-        print(character + " "*(width - 2) + character)
-    print(character * width)
+    try:        
+        if len(character) != 1:
+            raise Exception("'character' needs to be a string of length 1")
+        if (width < 2) or (height < 2):
+            raise Exception('"width" and "height" must be greater or equal to 2.')
+        print(character * width)
+        for i in range(height - 2):
+            print(character + " "*(width - 2) + character)
+        print(character * width)
+    except:
+      errorFile = open('error_log.txt', 'a')  
+      errorFile.write(traceback.format_exc())
+      errorFile.close()
+      print('The traceback info was written to error_log.txt')
 
-makeBox("##",5,125)
+makeBox("%",11,12)
     
