1d40bc6f83a0fd30ddb447929f67b56e9bda2adb
John R. Forbes <jrforbes24@gmail.com>
2023-03-11 11:26:35 -0700

    playing with some more logging

--- factorial.py
+++ factorial.py
@@ -1,2 +1,22 @@
 import logging
-logging.basicConfig()
+logging.basicConfig(filename='factorialLogging.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# logging.disable(logging.DEBUG)
+
+logging.debug('Start of program')
+
+total = 0
+
+def factorial(n):
+    total = 1
+    logging.debug('Start of the factorial (%s)' % (n))
+    for i in range(1,n + 1):
+        total = total * i
+        logging.debug('i is %s, total is %s' % (i, total))
+        
+    logging.info('Return value is %s' % (total))
+    return total
+
+total = factorial(7)
+# print(factorial(1000))
+print('The type of total:', type(total), ' The total:', str(total))
+logging.debug('End of program')
