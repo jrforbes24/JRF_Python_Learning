3ab368015e5da3c8edd6849230df3cef21928039
John R. Forbes <jrforbes24@gmail.com>
2023-03-15 08:20:03 -0600

    more play with web requests

--- webRequests.py
+++ webRequests.py
@@ -4,5 +4,8 @@ statusCode = theReturnedInfo.status_code
 theReturnedInfo.raise_for_status()
 if statusCode == 200:
     print('The length of the string:' , str(len(theReturnedInfo.text)))
-    playFile = open()
+    playFile = open('RomeoAndJuliet.txt','wb')
+    for chunk in theReturnedInfo.iter_content(100000):
+        playFile.write(chunk)
+    playFile.close
     
