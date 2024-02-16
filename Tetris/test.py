e4be2964c6886000004f4b00141003403fbf9918
John R. Forbes <jrforbes24@gmail.com>
2024-02-16 08:39:11 -0700

    working on tetris and list comprehensions.

---
+++ Tetris/test.py
@@ -0,0 +1,2 @@
+grid = [[("Lucie and Lillie are friends") for _ in range(10)] for _ in range(20)]
+print(grid)
--- Tetris/tetris.py
+++ Tetris/tetris.py
@@ -139,10 +139,22 @@ shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 16
  
  
 class Piece(object):
-    pass
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+        self.color = shape_colors[shapes.index(shape)]
+        self.rotation = 0
  
 def create_grid(locked_positions={}):
-    pass
+    grid = [[(0,0,0) for _ in range(10)] for _ in range(20)]
+    
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+             if (j,i) in locked_positions:
+                 c = locked_positions[(j,i)]
+                 grid[i][j] = c
+    return grid 
  
 def convert_shape_format(shape):
     pass
