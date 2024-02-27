5415962c683e157f9473d12a2969ef1af96566c1
John R. Forbes <jrforbes24@gmail.com>
2024-02-27 07:13:13 -0700

    Finished first version of the photo editor

--- ImageEditor/photoEditor.py
+++ ImageEditor/photoEditor.py
@@ -1,8 +1,18 @@
 from PIL import Image, ImageEnhance, ImageFilter
 import os
 
-path = 'ImageEditor\imgs'
-pathOut = 'ImageEditor\editedImgs'
+path = 'ImageEditor\\imgs'
+pathOut = 'ImageEditor\\editedImgs'
 
 for filename in os.listdir(path):
-    print(filename)
+    print(f"{path}\\{filename}")
+    img = Image.open(f"{path}\\{filename}")
+    edit = img.filter(ImageFilter.SHARPEN)
+    factor = 1.5
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(factor)
+    # clean_name splits the file path and takes the root. 
+    clean_name = os.path.splitext(filename)[0]    
+    edit.save(f'{pathOut}\\{clean_name}_edited.jpg')
+    
+    
