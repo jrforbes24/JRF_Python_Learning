4283471ba7f21f25f4a13bac49dc324c8acb21f7
JohnF <jrforbes24@gmial.com>
2025-09-27 14:27:22 -0600

    having fun

--- editWord.py
+++ editWord.py
@@ -25,9 +25,9 @@ for i in range(1, 11):
 
 
 records = (
-    (3, '101', 'Spam'),
+    (3, '101', 'bacon'),
     (7, '422', 'Eggs'),
-    (4, '631', 'Spam, spam, eggs, and spam')
+    (4, '6361', 'Spam, spam, egges, and spam')
 )
 
 table = document.add_table(rows=1, cols=3)
---
+++ jsSample.js
@@ -0,0 +1,4 @@
+console.log("___________________________");
+console.log("Rise & Shine!");
+console.log("Ready for a new day!!");
+console.log("___________________________");
